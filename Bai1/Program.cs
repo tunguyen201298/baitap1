5de2238fb323f70fd0d662f5d9c6e8ae057fba98
tunguyen98 <tudtdt1998@gmail.com>
2020-04-18 22:47:11 +0700

    - init project

---
+++ Bai1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Bai1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.OutputEncoding = Encoding.Unicode;
+            string name;
+            string strAge;
+            Console.WriteLine("Nhập họ tên:");
+            name = Console.ReadLine();
+
+            string pattern = @"^[a-zA-Z\s]+$";
+
+            Regex regex = new Regex(pattern);
+
+            while (!regex.IsMatch(name))
+            {
+                Console.Write("Nhập lại tên: ");
+                name = Console.ReadLine();
+            }
+
+            Console.WriteLine("Nhập tuổi:");
+            strAge = Console.ReadLine();
+
+            int age = 0;
+            while (!int.TryParse(strAge, out age))
+                {
+                    Console.Write("Nhập lại tuổi: ");
+                strAge = Console.ReadLine();
+                }
+
+            Console.WriteLine("Họ tên: {0,-10}, Tuổi: {1,-5}", name, age);
+                
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Bai2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            try
+            {
+                int number;
+                int fail = 1;
+                Random randoms = new Random();
+                int random = randoms.Next(1, 10);
+                Console.WriteLine("TRÒ CHƠI ĐOÁN SỐ");
+                Console.WriteLine("----------------");
+                while (fail <= 3)
+                {
+                    Console.WriteLine("Số bạn đoán là:");
+                    number = int.Parse(Console.ReadLine());
+                    if (number > 10)
+                        Console.WriteLine("Số nhập vào phải nhỏ hơn 10");
+                    else
+                    { 
+                        if (number == random)
+                        {
+                            Console.WriteLine("Bạn đã đoán đúng!!!-_-");
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Sai rồi!!!");
+                            fail++;
+                        }
+                    }
+                }
+                if (fail > 3)
+                {
+                    Console.WriteLine("Bạn thua rồi");
+                    Console.WriteLine("Kết quả là:" + random);
+                }
+                throw new Exception();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Dữ liệu nhập vào phải là số");
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
